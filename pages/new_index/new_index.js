5f8827c03c8714061f1e268b106e75be390646d8
zero <1137774080@qq.com>
2019-05-24 07:45:08 +0800

    更新

--- pages/new_index/new_index.js
+++ pages/new_index/new_index.js
@@ -12,7 +12,7 @@ Page({
     autoplay: true,
     indicatorColor: 'rgba(255, 255, 255, 1)',
     indicatorActiveColor: 'rgba(69, 208, 174, 1)',
-    interval: 1000,
+    interval: 3000,
     duration: 1000,
     page:1
   },
